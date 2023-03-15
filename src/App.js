b200aa1124e318eede5554e66769314481339197
Bright Iortsor <iortsor@gmail.com>
2023-03-15 16:15:05 +0100

    added a theme colorPicker

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Profile from "./components/Profile";
 import Login from "./components/Login";
-// import ChangeColor from "./components/ChangeColor";
+import ChangeColor from "./components/ChangeColor";
 
 function App() {
   return (
     <div className="App">
       <Profile />
       <Login />
-      {/* <ChangeColor /> */}
+      <ChangeColor />
     </div>
   );
 }
