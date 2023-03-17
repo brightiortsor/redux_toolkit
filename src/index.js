d5c9d249e7cc270d0ea7b31c2d499c8417c651bd
Bright Iortsor <iortsor@gmail.com>
2023-03-17 10:26:05 +0100

    added index css

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import userReducer from "./features/user";
-import themeReducer from "./features/theme";
-
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-    theme: themeReducer,
-  },
-});
+import { store } from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
