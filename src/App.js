8026e6e264306f69c71b4f0361f24ba9c1b87609
Bright Iortsor <iortsor@gmail.com>
2023-03-17 10:33:31 +0100

    added items to App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import "./App.css";
-import Profile from "./components/Profile";
-import Login from "./components/Login";
-import ChangeColor from "./components/ChangeColor";
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+import Modal from "./components/Modal";
 
 function App() {
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems("random"));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <Profile />
-      <Login />
-      <ChangeColor />
-    </div>
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
   );
 }
-
 export default App;
